b29ed87ed4c4d7ec0f56d14a700a51244ce1c915
paulohenriqueo <paulohenrique300305@gmail.com>
2024-06-24 12:16:07 -0300

    :tada: init: first commit

---
+++ professional/src/main/java/com/fatec/professional/ProfessionalApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.professional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessionalApplication.class, args);
+	}
+
+}
---
+++ professional/src/test/java/com/fatec/professional/ProfessionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.professional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
